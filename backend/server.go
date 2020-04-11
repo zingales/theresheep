67d2903b9ab72347e8c9707a5e8a7d025f7a0f7b
Evan Thomas <evan@tempoautomation.com>
2020-04-11 09:12:48 -0500

    Make AssignRolePool use json

--- backend/server.go
+++ backend/server.go
@@ -7,7 +7,6 @@ import (
 	"log"
 	"net/http"
 	"strconv"
-	"strings"
 	"sync"
 
 	"github.com/gorilla/pat"
@@ -96,6 +95,7 @@ var GameNotFoundError error = errors.New("Game with that id was not found")
 var NotImplementedError error = errors.New("That method has not been implemented yet")
 var MissingParamInUrlError error = errors.New("Request Url was missing a param key")
 var RoleNoteFoundError error = errors.New("This role does not exist")
+var InvalidJSON error = errors.New("Invalid Json")
 
 // Var Method Types:
 const MethodGet = "GET"
@@ -153,17 +153,17 @@ var roleCast = map[string]gamelogic.Role{
 }
 
 func AssignRolePool(game *gamelogic.Game, responseWriter http.ResponseWriter, request *http.Request) (error, int) {
-	err, rolesString := getFirstOccuranceInUrlParam(request, "roles")
-	if err != nil {
-		return err, http.StatusBadRequest
+	rolesReq := struct{ Roles []string }{}
+
+	if err := json.NewDecoder(request.Body).Decode(&rolesReq); err != nil {
+		return InvalidJSON, http.StatusBadRequest
 	}
 
 	var roles []gamelogic.Role
 
-	for _, value := range strings.Split(rolesString, ",") {
-		role, ok := roleCast[strings.ToLower(value)]
+	for _, roleName := range rolesReq.Roles {
+		role, ok := gamelogic.RoleNameToID[roleName]
 		if !ok {
-			log.Print(value)
 			return RoleNoteFoundError, http.StatusBadRequest
 		}
 		roles = append(roles, role)
@@ -228,7 +228,7 @@ func CreatePlayer(game *gamelogic.Game, responseWriter http.ResponseWriter, requ
 	player := struct{ Name string }{}
 
 	if err := json.NewDecoder(request.Body).Decode(&player); err != nil {
-		return err, http.StatusBadRequest
+		return errors.New("Invalid json"), http.StatusBadRequest
 	}
 
 	id, err := game.AddPlayer(
